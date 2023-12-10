86c6b7fb8d25598a8879e94b40da3e96bc44e1e4
JRF <>
2023-12-10 04:17:46 -0600

    WIP "first" pass at the JS
    still doesn't work for unknown reasons.
    guess how many times i've tried & failed to get the JS to even load.
    god *DAMN* i've forgotten how much i hate writing userscripts

--- Quick&Dirty Instant Thread Collapsation.user.js
+++ Quick&Dirty Instant Thread Collapsation.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Quick&Dirty Instant Thread Collapsation
-// @version  0.0.2
+// @version  0.0.3
 // @match    https://instant.leet.nu/room/*
 // ==/UserScript==
 
@@ -24,4 +24,34 @@ var style = document.createElement('style')
 style.textContent = css
 document.head.appendChild(style)
 
-// TODO: add class is-hidden to replies of collapsed thread
+var js = `
+document.addEventListener("DOMContentLoaded", () => {
+window.$onload(() => {
+
+Instant.listen('message.click', evt => {
+	var c = evt.source // click event
+	var m = c.target // message element
+
+	/* Filter out clicks on links */
+	if (m.matches('a, a *')) return
+
+	if (c.shiftKey && !c.ctrlKey && !c.altKey && !c.metaKey) {
+		evt.cancel()
+		var hideThread = m.classList.toggle('hide-thread')
+		// Instant uses .is-hidden to determine if keyboard navigation is prevented
+		m.querySelector('.replies').childNodes.forEach(el => {
+			// don't remove is-hidden from /data commands
+			if (el.matches('.message:not(.data)')) {
+				el.classList.toggle('is-hidden', hideThread)
+			}
+		})
+	}
+})
+
+}) // end $onload
+}) // end DOMContentLoaded
+`
+
+var script = document.createElement('script')
+script.textContent = js
+document.head.appendChild(script)
