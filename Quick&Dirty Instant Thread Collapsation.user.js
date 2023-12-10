4c481e25756cc4e997c7e02a58e4438ba79a9d85
JRF <>
2023-12-10 05:39:54 -0600

    Update @description
    it's not a bug, it's a feature!

--- Quick&Dirty Instant Thread Collapsation.user.js
+++ Quick&Dirty Instant Thread Collapsation.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Quick&Dirty Instant Thread Collapsation
-// @description   Ctrl+click a message to hide/show its replies
+// @description   Ctrl+click a message to hide/show its replies (n.b. messages' replies can be preemptively hidden before they have any)
 // @version       0.1.3
 // @match         https://instant.leet.nu/room/*
 // ==/UserScript==
